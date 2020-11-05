7e93a5a90087c09f638cfd34f29292f0dcfa1a14
Alexey Popov <alexey.popov@motorsport.com>
2020-11-05 15:55:06 +0300

    About us page

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="footer">
     <div class="container container_fixed footer__container">
         <div class="footer-col">
-            <div class="d-flex">
+            <div class="d-flex align-items-center">
                 <img src="<?= get_template_directory_uri() ?>/images/logo.png" class="footer__logo" alt="logo">
                 <p>SM School
                     <br>Все права защищены © <?= date('Y') ?>
--- index.php
+++ index.php
@@ -6,10 +6,11 @@ if (have_posts()):
     $thumbnail_id = get_post_thumbnail_id();
     ?>
     <div class="container container_fixed container_full-width_m-less content-box">
-        <?php if ($thumbnail_id): ?>
+        <?php
+        the_title('<h1>', '</h1>');
+        if ($thumbnail_id): ?>
             <img src="<?php the_post_thumbnail_url('large'); ?>" alt="thumbnail" class="feature-image">
         <?php endif; ?>
-        <?php the_title('<h1>', '</h1>') ?>
         <div class="content <?= $thumbnail_id ? 'content_have-feature-image' : '' ?>">
             <?php the_content(); ?>
         </div>
