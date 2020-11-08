e5ce72d9e3d9fe40966b2bd56fff1f2beb3e4100
Alexey Popov <alexey.popov@motorsport.com>
2020-11-08 23:44:49 +0300

    Updated courses styles

--- archive.php
+++ archive.php
@@ -4,8 +4,10 @@ get_header();
 if (have_posts()):
     $title = get_post_type_labels(get_post_type_object(get_post_type()))->archives;
     ?>
-    <div class="container container_fixed">
+    <div class="container container_fixed container_full-width_m-less">
         <h1><?= $title ?></h1>
+    </div>
+    <div class="container container_fixed">
         <div class="content-columns">
             <?php
             while (have_posts()):
--- template_parts/content-course.php
+++ template_parts/content-course.php
@@ -1,13 +1,19 @@
 <?php
 
 $category = get_the_category();
-$link = get_field('link');
+$link = get_field('other_fields')['link'];
+$cost = number_format(get_field('other_fields')['cost'] ?? 0, 0, ',', ' ') . ' &#8381;';
 ?>
 <div class="card">
-    <?= $category[0]->cat_name ?? '' ?>
-    <?= the_title() ?>
-    <?= get_avatar(get_the_author_meta('ID'), 80) ?>
-    <?= get_the_author(); ?>
-    <a href="<?= $link ?>" target="_blank">Смотреть курс <img
+    <div class="card__panel">
+        <div class="card__category"><?= $category[0]->cat_name ?? 'Без категории' ?></div>
+        <div class="card__title"><?= the_title() ?></div>
+        <div class="card__author">
+            <?= get_avatar(get_the_author_meta('ID'), 80) ?>
+            <span><?= get_the_author(); ?></span>
+        </div>
+    </div>
+    <span class="card__cost"><?= $cost ?></span>
+    <a href="<?= $link ?>" class="card__link" target="_blank">Смотреть курс <img
                 src="<?= get_template_directory_uri() ?>/images/icons/arrow.png" alt="arrow"></a>
 </div>
