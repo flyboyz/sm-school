dbf379cd131651a4f6feb66a81d30529854df1e0
Alexey Popov <alexey.popov@motorsport.com>
2020-12-01 17:44:27 +0300

    Stylish Teacher single page

--- single-teacher.php
+++ single-teacher.php
@@ -1,4 +1,5 @@
 <?php
+$fields = get_field('teacher_fields');
 
 get_header();
 if (have_posts()):
@@ -10,20 +11,24 @@ if (have_posts()):
     ]);
     ?>
     <div class="container container_fixed container_full-width_m-less content-box">
-        <?php
-        the_title('<h1>', '</h1>'); ?>
-        <div class="content">
-            <?php
-            foreach ($users as $user): ?>
+        <a href="/teachers/" class="link-back icon icon_block icon-arrow icon-arrow_back">Вернуться к списку
+            преподавателей</a>
+        <div class="details-card">
+            <div class="card">
                 <?php
-                $userdata = get_user_meta($user->ID);
-                var_dump($userdata['description'][0]); ?>
+                the_post_thumbnail('medium');
+                ?>
+                <div class="card__title"><?= get_the_title(); ?></div>
+                <div class="card__subtitle"><?= $fields['position'] ?></div>
+                <div class="card__actions">
+                    <a href="/" class="card__link icon icon-arrow">Курсы</a>
+                    <a href="/" class="card__link icon icon-arrow">Статьи</a>
+                </div>
+            </div>
+            <div class="details-content">
                 <?php
-                var_dump($user->data->user_login); ?>
-            <?php
-            endforeach; ?>
-            <?php
-            the_content(); ?>
+                the_content(); ?>
+            </div>
         </div>
     </div>
 <?php
