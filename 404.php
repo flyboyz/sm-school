dced5594a730f546b68e629952203477b76fc87c
Alexey Popov <alexey.popov@motorsport.com>
2020-12-19 21:22:49 +0300

    Updated 404 page styles

--- 404.php
+++ 404.php
@@ -3,7 +3,11 @@
 get_header();
 ?>
     <div class="container container_fixed container_no-paddnig content-box">
-        404
+        <div class="error-box">
+            <p class="error-code">404</p>
+            <p>Такой страницы не существует</p>
+            <a href="/">Перейти на главную</a>
+        </div>
     </div>
 <?php
 get_footer();
