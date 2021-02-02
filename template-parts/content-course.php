915f7e9c45dddf6dba31c7d393c5db036e950ee9
Alexey Popov <alexey.popov@motorsport.com>
2021-02-02 20:24:20 +0300

    Added Course page

--- template-parts/content-course.php
+++ template-parts/content-course.php
@@ -12,6 +12,5 @@ $teacher = get_the_teacher();
         </div>
     </div>
     <span class="card__cost"><?= get_course_cost() ?></span>
-    <a href="<?= get_field('fields')['link'] ?>" class="card__title icon icon-arrow" target="_blank">Смотреть
-        курс</a>
+    <a href="<?= get_the_permalink() ?>" class="card__title icon icon-arrow">Смотреть курс</a>
 </div>
