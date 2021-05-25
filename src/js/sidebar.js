3b7a36f15517d457a8ee7c36d0769ce7aa6f1a1f
Alexey Popov <alexey.popov@motorsport.com>
2021-05-25 22:20:59 +0300

    Updated sidebar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,18 +1,20 @@
-'use strict'
+'use strict';
 
 const body = document.querySelector('body');
 const sidebarButton = $('#sidebarButton');
 
-if (sidebarButton !== null) {
-    $(sidebarButton).fancybox({
-        src: '#sidebar',
-        touch: false,
-        baseClass: 'fancybox-sidebar',
-        beforeShow: () => {
-            body.classList.add('sidebar-opened')
-        },
-        beforeClose: () => {
-            body.classList.remove('sidebar-opened')
-        }
-    });
+export default () => {
+    if (sidebarButton !== null) {
+        $(sidebarButton).fancybox({
+            src: '#sidebar',
+            touch: false,
+            baseClass: 'fancybox-sidebar',
+            beforeShow: () => {
+                body.classList.add('sidebar-opened')
+            },
+            beforeClose: () => {
+                body.classList.remove('sidebar-opened')
+            }
+        });
+    }
 }
