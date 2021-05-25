b530a26874c1830e1a578e03fe7f1855f4dcb49f
Alexey Popov <alexey.popov@motorsport.com>
2021-05-25 22:21:33 +0300

    Updated Course contents

--- template-parts/blocks/course-contents/course-contents.php
+++ template-parts/blocks/course-contents/course-contents.php
@@ -6,6 +6,9 @@
  * @param array $block The block settings and attributes.
  */
 
+global $post;
+$post_author_ID = (int)$post->post_author;
+
 $className = '';
 if (!empty($block['className'])) {
     $className .= ' ' . $block['className'];
@@ -21,10 +24,28 @@ echo esc_attr($className); ?>">
             <div class="swiper-wrapper">
                 <?php
                 if (!is_admin()):
-                    foreach ($contents as $content): ?>
+                    foreach ($contents as $content):
+                        $authors = $content['info']['authors'];
+
+                        if (empty($authors)) {
+                            $authors[] = $post_author_ID;
+                        }
+                        ?>
                         <div class="contents-list__item swiper-slide">
                             <span><?= $content['title'] ?></span>
                             <?= apply_filters('the_content', $content['text']) ?>
+                            <div class="info">
+                                <div class="time">
+                                    <?= file_get_contents(get_template_directory() . '/images/icons/clock.svg') ?>
+                                    <?= $content['info']['time'] ?>
+                                </div>
+                                <div class="authors">
+                                    <?php
+                                    foreach ($authors as $author_ID) {
+                                        echo wp_get_attachment_image(get_fields("user_$author_ID")['avatar']);
+                                    } ?>
+                                </div>
+                            </div>
                         </div>
                     <?php
                     endforeach;
