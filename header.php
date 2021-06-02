c8478ccad76e1fedb27af57858396492092a8690
Alexey Popov <alexey.popov@motorsport.com>
2021-06-02 17:36:23 +0300

    Added Yandex.Metrika counter

--- header.php
+++ header.php
@@ -2,8 +2,18 @@
 <html <?php
 language_attributes(); ?>>
 <head>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript"> (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(80102551, "init", {clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true}); </script>
+    <!-- /Yandex.Metrika counter -->
     <meta charset="<?php
-    bloginfo('charset'); ?>"/>
+	bloginfo( 'charset' ); ?>"/>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
@@ -13,16 +23,21 @@ language_attributes(); ?>>
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-    <?php
-    wp_head(); ?>
+	<?php
+	wp_head(); ?>
 </head>
 <body <?php
 body_class(); ?>>
+<!-- Yandex.Metrika counter -->
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/80102551" style="position:absolute; left:-9999px;" alt=""/></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
 <?php
 wp_body_open();
-if (get_page_template_slug() === 'template-parts/products-list.php') {
-    get_template_part('template-parts/header', 'landing');
+if ( get_page_template_slug() === 'template-parts/products-list.php' ) {
+	get_template_part( 'template-parts/header', 'landing' );
 } else {
-    get_template_part('template-parts/header', 'standard');
+	get_template_part( 'template-parts/header', 'standard' );
 } ?>
 <main>
