f106d30f8b4e6af27d30177a4919ea0f53d48688
Alexey Popov <alexey.popov@motorsport.com>
2021-06-04 21:54:33 +0300

    Added button animation

--- src/js/form.js
+++ src/js/form.js
@@ -1,18 +1,19 @@
-'use strict';
+'use strict'
 
-import Message from "./message";
+import Message from './message'
 
 export default () => {
-  let sendpulseForms = document.querySelectorAll('.sendpulse-form');
+  let sendpulseForms = document.querySelectorAll('.sendpulse-form')
 
   if (sendpulseForms !== null) {
     sendpulseForms.forEach(form => {
       form.addEventListener('submit', function (e) {
         const formData = new FormData(form)
 
-        e.preventDefault();
+        e.preventDefault()
 
-        form.querySelector('[type="submit"]').setAttribute('disabled', 'disabled');
+        form.querySelector('[type="submit"]').setAttribute('disabled', 'disabled')
+        form.querySelector('[type="submit"]').classList.add('progress-btn')
 
         fetch(backend_data.ajaxurl, {
           method: 'POST',
@@ -21,36 +22,36 @@ export default () => {
         })
           .then((response) => response.json())
           .then(function (data) {
-            let goalName = form.getAttribute('data-reach-goal');
+            let goalName = form.getAttribute('data-reach-goal')
 
-            checkAndReachGoal(goalName);
+            checkAndReachGoal(goalName)
 
             if (data.is_error) {
-              $.fancybox.open(Message('Ошибка отправки', 'Сообщите об этом администратору'));
+              $.fancybox.open(Message('Ошибка отправки', 'Сообщите об этом администратору'))
             } else {
               if (form.hasAttribute('data-form-submit')) {
-                form.submit();
+                form.submit()
               } else {
-                $.fancybox.close();
+                $.fancybox.close()
 
-                form.querySelector('[type="submit"]').removeAttribute('disabled');
-                form.reset();
+                form.querySelector('[type="submit"]').removeAttribute('disabled')
+                form.reset()
 
                 if (form.classList.contains('sendpulse-form')) {
-                  $.fancybox.open(Message('Поздравляем', 'Вы успешно записались на мастер-класс!'));
+                  $.fancybox.open(Message('Поздравляем', 'Вы успешно записались на мастер-класс!'))
                 } else {
-                  $.fancybox.open(Message('Успешно', ''));
+                  $.fancybox.open(Message('Успешно', ''))
                 }
               }
             }
-          });
-      });
-    });
+          })
+      })
+    })
   }
 }
 
-function checkAndReachGoal(goalName) {
-  if (!!goalName && typeof ym === "function") {
-    ym(80102551, 'reachGoal', goalName);
+function checkAndReachGoal (goalName) {
+  if (!!goalName && typeof ym === 'function') {
+    ym(80102551, 'reachGoal', goalName)
   }
 }
