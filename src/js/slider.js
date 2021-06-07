b599c25fa1fbcb81c0cc4b14a6514dd71a26ae8a
Alexey Popov <alexey.popov@motorsport.com>
2021-06-07 12:31:42 +0300

    Moved packages to slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,41 +1,69 @@
-'use strict';
+'use strict'
 
-import Swiper, {Pagination} from "swiper/core";
+import Swiper, { Navigation, Pagination } from 'swiper/core'
 
 const attrs = new Map([[
-    'co-authors',
-    {
-        autoplay: false,
-        centeredSlides: true,
-        spaceBetween: 30,
-        slidesPerView: 1.85,
-    }
+  'co-authors',
+  {
+    autoplay: false,
+    centeredSlides: true,
+    spaceBetween: 30,
+    slidesPerView: 1.85,
+  }
 ], [
-    'contents-list',
-    {
-        autoplay: false,
-        spaceBetween: 20,
-        slidesPerView: 1.1,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            type: 'bullets',
-        },
-    }
+  'contents-list',
+  {
+    autoplay: false,
+    spaceBetween: 20,
+    slidesPerView: 1.1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      type: 'bullets',
+    },
+  }
+], [
+  'course-packages',
+  {
+    autoplay: false,
+    spaceBetween: 120,
+    slidesPerView: 2,
+    initialSlide: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  }
 ]])
 
 export default () => {
-    for (let [sliderName, attr] of attrs) {
-        let slider = `.${sliderName}`;
+  for (let [sliderName, attr] of attrs) {
+    let slider = `.${sliderName}`
 
-        if (document.querySelector(slider) !== null) {
-            let ss;
+    if (document.querySelector(slider) !== null) {
+      let ss
 
-            Swiper.use([Pagination]);
+      Swiper.use([Pagination, Navigation])
 
-            if ((sliderName === 'co-authors' || sliderName === 'contents-list') && window.matchMedia('(max-width:767px)').matches) {
-                ss = new Swiper(slider, attr);
-            }
+      if ((sliderName === 'co-authors' || sliderName === 'contents-list') && window.matchMedia('(max-width:767px)').matches) {
+        ss = new Swiper(slider, attr)
+      } else if (sliderName === 'course-packages') {
+        const slidesCount = document.querySelectorAll(`${slider} .swiper-slide`).length
+
+        if (slidesCount === 1) {
+          attr.centeredSlides = true
         }
+
+        if (slidesCount < 3) {
+          attr.navigation = false
+
+          document.querySelectorAll(`${slider} [class*=swiper-button]`).forEach(function (item) {
+            item.remove()
+          })
+        }
+
+        new Swiper(slider, attr)
+      }
     }
+  }
 }
--- template-parts/section/packages.php
+++ template-parts/section/packages.php
@@ -4,68 +4,79 @@ $packages = get_field('packages');
 if (!empty($packages)):
     ?>
     <div class="section section_course-packages">
-        <?php
-        $i = 0;
-        foreach ($packages as $package_key => $package):
-            if ($package['is_active']):
-                $package_type = $package['type_group']['type'];
-                $package_cost = get_cost($package['cost'] ?: 0);
-                $package_full_description = $package['description']['full'];
-
-                $details_key = "$package_key-details";
-                $package['key'] = $package_key;
-
-                echo $i % 2 ? '' : '<div>';
-                ?>
-                <div class="course-package">
+        <div class="container container_fixed container_full-width_s-less">
+            <div class="course-packages">
+                <div class="swiper-wrapper">
                     <?php
-                    if ($package['icon']): ?>
-                        <img src="<?= $package['icon']['sizes']['thumbnail'] ?>" alt="icon" class="package-icon">
-                    <?php
-                    endif; ?>
-                    <h3 class="name">Тариф <br>"<?= $package['name'] ?>"</h3>
-                    <div class="description">
-                        <?= $package['description']['short'] ?>
-                        <?php
-                        if ($package_full_description): ?>
-                            <div class="more">
-                                <a data-fancybox data-src="#package-<?= $details_key ?>"
-                                   data-options='{"touch" : false}'
-                                   href="javascript:;">узнать подробнее...</a>
+                    foreach ($packages as $package_key => $package):
+                        if ($package['is_active']):
+                            $package_type = $package['type_group']['type'];
+                            $package_cost = get_cost($package['cost'] ?: 0);
+                            $package_full_description = $package['description']['full'];
+
+                            $details_key = "$package_key-details";
+                            $package['key'] = $package_key;
+                            ?>
+                            <div class="course-package swiper-slide">
+                                <?php
+                                if ($package['icon']): ?>
+                                    <img src="<?= $package['icon']['sizes']['thumbnail'] ?>" alt="icon"
+                                         class="package-icon">
+                                <?php
+                                endif; ?>
+                                <h3 class="name">Тариф <br><b>"<?= $package['name'] ?>"</b></h3>
+                                <div class="description">
+                                    <?= $package['description']['short'] ?>
+                                    <?php
+                                    if ($package_full_description): ?>
+                                        <div class="more">
+                                            <a data-fancybox data-src="#package-<?= $details_key ?>"
+                                               data-options='{"touch" : false}'
+                                               href="javascript:;">узнать подробнее...</a>
+                                        </div>
+                                    <?php
+                                    endif; ?>
+                                </div>
+                                <div class="cost"><?= $package_cost ?></div>
+                                <a data-fancybox data-src="#Modal_<?= $package_key ?>" data-options='{"touch" : false}'
+                                   href="javascript:;"
+                                   class="button button_lighting"><?= $package_type === 'sendpulse' ? 'Записаться' : 'Купить' ?></a>
+                                <?php
+                                if ($package_type !== 'sendpulse'):
+                                    ?>
+                                    <img src="<?= get_template_directory_uri() ?>/images/icons/cards.png" alt="card"
+                                         class="cards">
+                                <?php
+                                endif;
+                                ?>
                             </div>
-                        <?php
-                        endif; ?>
-                    </div>
-                    <div class="cost"><?= $package_cost ?></div>
-                    <a data-fancybox data-src="#Modal_<?= $package_key ?>" data-options='{"touch" : false}'
-                       href="javascript:;"
-                       class="button button_lighting"><?= $package_type === 'sendpulse' ? 'Записаться' : 'Купить' ?></a>
-                    <?php
-                    if ($package_type !== 'sendpulse'):
-                        ?>
-                        <img src="<?= get_template_directory_uri() ?>/images/icons/cards.png" alt="card" class="cards">
-                    <?php
-                    endif;
+                            <?php
+                            if ($package_full_description): ?>
+                                <div class="container container_l modal modal_light modal_package"
+                                     id="package-<?= $details_key ?>"
+                                     style="display: none;">
+                                    <div class="name">Тариф "<?= $package['name'] ?>"</div>
+                                    <div class="description">
+                                        <?= $package['description']['full'] ?>
+                                    </div>
+                                </div>
+                            <?php
+                            endif;
+                            get_template_part('template-parts/form/' . $package_type, '', $package);
+                        endif;
+                    endforeach;
                     ?>
                 </div>
-                <?php
-                if ($package_full_description): ?>
-                    <div class="container container_l modal modal_light modal_package"
-                         id="package-<?= $details_key ?>"
-                         style="display: none;">
-                        <div class="name">Тариф "<?= $package['name'] ?>"</div>
-                        <div class="description">
-                            <?= $package['description']['full'] ?>
-                        </div>
-                    </div>
-                <?php
-                endif;
-                get_template_part('template-parts/form/' . $package_type, '', $package);
-                echo $i % 2 || $package_key === count($packages) - 1 ? '</div>' : '';
-                $i++;
-            endif;
-        endforeach;
-        ?>
+                <div class="swiper-button-prev">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/icons/slider-arrow.svg"
+                         alt="arrow-prev">
+                </div>
+                <div class="swiper-button-next">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/icons/slider-arrow.svg"
+                         alt="arrow-next">
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 endif;
