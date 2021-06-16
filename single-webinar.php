6b8f16a09274492e4f45991916af513f119b81f0
Alexey Popov <alexey.popov@motorsport.com>
2021-06-16 10:01:16 +0300

    Added label for empty webinar event_date

--- single-webinar.php
+++ single-webinar.php
@@ -1,17 +1,18 @@
 <?php
 
 get_header();
+$event_date = get_field( 'event_date', 'post_' . $post->ID );
 ?>
-    <div class="container container_fixed event-date-box">
-        <div class="event-date"><?= wp_maybe_decline_date(get_field('event_date', 'post_' . $post->ID)) ?></div>
-    </div>
-
+	<div class="container container_fixed event-date-box">
+		<div
+			class="event-date"><?= $event_date ? wp_maybe_decline_date( $event_date ) : get_field( 'event_date_label' ) ?></div>
+	</div>
 <?php
 the_content();
 
-get_template_part('template-parts/section/co-authors');
-get_template_part('template-parts/section/feedback');
-get_template_part('template-parts/section/faq');
+get_template_part( 'template-parts/section/co-authors' );
+get_template_part( 'template-parts/section/feedback' );
+get_template_part( 'template-parts/section/faq' );
 
 get_footer();
 get_template_part( 'template-parts/section/chatra' );
