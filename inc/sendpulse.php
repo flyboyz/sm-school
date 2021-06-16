88372ae1b42e3bf903080657b934794efdcf5587
Alexey Popov <alexey.popov@motorsport.com>
2021-06-16 08:46:36 +0300

    Updated SendPulse select fill method

--- inc/sendpulse.php
+++ inc/sendpulse.php
@@ -7,26 +7,22 @@ use Sendpulse\RestApi\Storage\SessionStorage;
  * @throws Exception
  */
 function acf_load_list_address_books( $field ) {
-	$post_types = array( 'course', 'webinar' );
+	$SPApiClient = new ApiClient( API_USER_ID, API_SECRET, new SessionStorage() );
+	$list        = $SPApiClient->listAddressBooks();
 
-	if ( is_admin() && in_array( get_current_screen()->post_type, $post_types ) ) {
-		$SPApiClient = new ApiClient( API_USER_ID, API_SECRET, new SessionStorage() );
-		$list        = $SPApiClient->listAddressBooks();
-
-		$field['choices'] = [];
-		if ( is_array( $list ) ) {
-			foreach ( $list as $book ) {
-				if ( $book->status === 0 ) {
-					$field['choices'][ $book->id ] = $book->name;
-				}
+	$field['choices'] = [];
+	if ( is_array( $list ) ) {
+		foreach ( $list as $book ) {
+			if ( $book->status === 0 ) {
+				$field['choices'][ $book->id ] = $book->name;
 			}
 		}
 	}
 
-	return $field ?? array();
+	return $field;
 }
 
-add_filter( 'acf/load_field/name=list_address_books', 'acf_load_list_address_books' );
+add_filter( 'acf/prepare_field/name=list_address_books', 'acf_load_list_address_books' );
 
 
 /**
