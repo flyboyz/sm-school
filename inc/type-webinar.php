ba65fe68775111b6222393500d4486e011531463
Alexey Popov <alexey.popov@motorsport.com>
2021-06-17 10:15:29 +0300

    Renamed 'webinar' slug to 'training'

--- inc/type-webinar.php
+++ inc/type-webinar.php
@@ -1,40 +1,43 @@
 <?php
 
-function register_webinar_post_type()
-{
-    register_post_type('webinar', [
-        'label' => null,
-        'labels'          => [
-            'name' => 'Тренинг',
-            'singular_name' => 'Тренинг',
-            'add_new' => 'Добавить тренинг',
-            'add_new_item' => 'Добавление тренинга',
-            'edit_item' => 'Редактирование тренинга',
-            'new_item' => 'Новый тренинг',
-            'view_item' => 'Смотреть тренинг',
-            'search_items' => 'Искать тренинг',
-            'not_found' => 'Не найдено',
-            'not_found_in_trash' => 'Не найдено в корзине',
-            'parent_item_colon' => '',
-            'menu_name' => 'Тренинги',
-            'archives' => 'Тренинги'
-        ],
-        'description'     => '',
-        'public'          => true,
-        'show_in_menu'    => null,
-        'show_in_rest'    => true,
-        'rest_base'       => null,
-        'menu_position'   => 4,
-        'menu_icon'       => 'dashicons-media-video',
-        'hierarchical'    => false,
-        'supports'        => ['title', 'editor', 'thumbnail', 'author'],
-        'taxonomies'      => ['category'],
-        'has_archive'     => true,
-        'rewrite'         => true,
-        'query_var'       => true,
-        'capability_type' => 'webinar',
-        'map_meta_cap'    => true,
-    ]);
+function register_webinar_post_type() {
+	register_post_type( 'webinar', [
+		'label'           => null,
+		'labels'          => [
+			'name'               => 'Тренинг',
+			'singular_name'      => 'Тренинг',
+			'add_new'            => 'Добавить тренинг',
+			'add_new_item'       => 'Добавление тренинга',
+			'edit_item'          => 'Редактирование тренинга',
+			'new_item'           => 'Новый тренинг',
+			'view_item'          => 'Смотреть тренинг',
+			'search_items'       => 'Искать тренинг',
+			'not_found'          => 'Не найдено',
+			'not_found_in_trash' => 'Не найдено в корзине',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Тренинги',
+			'archives'           => 'Тренинги'
+		],
+		'description'     => '',
+		'public'          => true,
+		'show_in_menu'    => null,
+		'show_in_rest'    => true,
+		'rest_base'       => null,
+		'menu_position'   => 4,
+		'menu_icon'       => 'dashicons-media-video',
+		'hierarchical'    => false,
+		'supports'        => [ 'title', 'editor', 'thumbnail', 'author' ],
+		'taxonomies'      => [ 'category' ],
+		'has_archive'     => 'training',
+		'rewrite'         => [
+			'slug'       => 'training',
+			'with_front' => true,
+		],
+		'query_var'       => true,
+		'capability_type' => 'webinar',
+		'map_meta_cap'    => true,
+		''
+	] );
 }
 
-add_action('init', 'register_webinar_post_type');
+add_action( 'init', 'register_webinar_post_type' );
