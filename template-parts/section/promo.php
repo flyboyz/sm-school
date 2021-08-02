756dbc1389513a29a96577377b9f2d9da8085653
Alexey Popov <alexey.popov@motorsport.com>
2021-08-02 17:44:54 +0300

    Promo text update

--- template-parts/section/promo.php
+++ template-parts/section/promo.php
@@ -1,9 +1,17 @@
+<?php
+
+$text = 'lesson' === get_post_type() ? 'Урок доступен до ' : 'Спеццена доступна до ';
+?>
 <div class='promo'>
     <div class="container container_fixed">
 		<span><?= file_get_contents( get_template_directory() . '/images/icons/clock.svg' ) ?>
 			Осталось <?php
 			do_action( 'promo_time_left' ); ?></span>
-        <span>Страница доступна до <b><?php
-				do_action( 'promo_end_date' ); ?></b></span>
+        <span>
+            <?php
+            echo $text; ?>
+            <b><?php
+	            do_action( 'promo_end_date' ); ?></b>
+        </span>
     </div>
 </div>
